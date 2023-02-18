f5a343c0564e1ad73e33b38f97a76d0be53bd857
Dereje Getahun <getahun.d@gmail.com>
2023-02-18 17:43:09 +0300

    move to src folder and remove global User var

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,11 +1,13 @@
 /* eslint-disable import/no-import-module-exports */
 import { Express } from 'express';
+import { Model } from 'mongoose';
 // Index for authentication
 import passport from 'passport';
 import { setAuthRoutes } from './routes';
 import { passportConfig } from './passportConfig';
+import { UserType } from '../interfaces';
 
-const configEntry = (app: Express, User: any) => {
+const configEntry = (app: Express, User: Model<UserType>) => {
   passportConfig(passport, User); // pass passport for configuration
   app.use(passport.initialize());
   app.use(passport.session()); // persistent login sessions
--- src/auth/passportConfig.ts
+++ src/auth/passportConfig.ts
@@ -1,28 +1,29 @@
 import { PassportStatic } from 'passport';
+import { Model } from 'mongoose';
 // config/passport.js for twitter
 import { Strategy as TwitterStrategy, Profile as twitterProfile } from 'passport-twitter';
 import { OAuth2Strategy as GoogleStrategy, Profile as googleProfile, VerifyFunction } from 'passport-google-oauth';
 import { Strategy as GitHubStrategy, Profile as githubProfile } from 'passport-github';
 // load up the user model
 import { getApiKeys } from '../utils';
-
-let PassedUser: any;
+import { UserType } from '../interfaces';
 
 export const processLogin = async (
   token: string,
   tokenSecret: string,
   profile: twitterProfile | googleProfile | githubProfile,
   done: VerifyFunction,
+  User: Model<UserType>
 ) => {
   const {
     provider, id, username, displayName, emails,
   } = profile;
   try {
-    const user = await PassedUser.findOne({ $and: [{ userId: id }, { service: provider }] }).exec();
+    const user = await User.findOne({ $and: [{ userId: id }, { service: provider }] }).exec();
     if (user) {
       return done(null, user);
     }
-    const newUser = await PassedUser.create({
+    const newUser = await User.create({
       userId: id,
       token,
       username: username || (emails && emails[0].value),
@@ -35,8 +36,7 @@ export const processLogin = async (
   }
 };
 
-export const passportConfig = (passport: PassportStatic, User: any) => {
-  PassedUser = User;
+export const passportConfig = (passport: PassportStatic, User: Model<UserType>) => {
   const { keys: { twitterApiKeys, googleApiKeys, githubApiKeys } } = getApiKeys();
 
   // used to serialize the user for the session
@@ -52,14 +52,14 @@ export const passportConfig = (passport: PassportStatic, User: any) => {
   });
 
   if (twitterApiKeys) {
-    passport.use(new TwitterStrategy(twitterApiKeys, processLogin));
+    passport.use(new TwitterStrategy(twitterApiKeys, (...args) => processLogin(...args, User)));
   }
 
   if (googleApiKeys) {
-    passport.use(new GoogleStrategy(googleApiKeys, processLogin));
+    passport.use(new GoogleStrategy(googleApiKeys, (...args) => processLogin(...args, User)));
   }
 
   if (githubApiKeys) {
-    passport.use(new GitHubStrategy(githubApiKeys, processLogin));
+    passport.use(new GitHubStrategy(githubApiKeys, (...args) => processLogin(...args, User)));
   }
 };
